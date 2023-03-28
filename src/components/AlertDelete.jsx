969a92aac7bb96db82b76c6d1e20d6db2d5946ed
vladpro42 <bondarev.vlad.01@gmail.com>
2023-03-28 19:45:19 +0300

    feat: add style AlertDelete.jsx

--- src/components/AlertDelete.jsx
+++ src/components/AlertDelete.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
-import { toggleAlertDeleteBoard, removeBoard } from '../slice/createBoardSlice'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleAlertDeleteBoard, removeBoard } from '../slice/createBoardSlice';
+import css from "../style/AlertDelete.module.css"
 
-const AlertDelete = ({id}) => {
+const AlertDelete = ({ id }) => {
 
     const dispatch = useDispatch()
 
     return (
-        <div className="alert">
-            <div className="alert__delete">
-                <h2 className='alert__title'>Вы уверены?</h2>
-                <button onClick={() => dispatch(removeBoard(id))} className='alert__button alert__button_delete'>Удалить</button>
-                <button onClick={() => dispatch(toggleAlertDeleteBoard("board"))} className='alert__button'>Отменить</button>
+        <div className={css.alert}>
+            <div className={css.alert__delete}>
+                <h2 className={css.alert__title}>Вы уверены?</h2>
+                <button onClick={() => dispatch(removeBoard(id))} className={[css.alert__button, css.alert__button_delete].join(" ")}>Удалить</button>
+                <button onClick={() => dispatch(toggleAlertDeleteBoard("board"))} className={css.alert__button}>Отменить</button>
             </div>
         </div>
     )
